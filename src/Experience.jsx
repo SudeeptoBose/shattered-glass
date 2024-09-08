489772c8c304b85ac3241f438f48353e2fb9dd19
SudeeptoBose <sudeeptobose23@gmail.com>
2024-09-08 19:48:09 +0600

    edits

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -5,12 +5,11 @@ import { Shattered } from './Shattered'
 const Experience = () => {
 	return (
 		<>
-		<Float>
-
-		<Text maxWidth={1} font='/BrokenGlass.ttf' textAlign='center' position={[0,0,-2]}>
-			Shattered Glass
-		</Text>
-		</Float>
+			<Float>
+				<Text maxWidth={1} font='/BrokenGlass.ttf' textAlign='center' position={[0, 0, -2]}>
+					Shattered Glass
+				</Text>
+			</Float>
 			<Float rotationIntensity={2}>
 				<Shattered />
 			</Float>
