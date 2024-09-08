fcf908a89f809abd16d71b9d0f855067f4437f55
SudeeptoBose <sudeeptobose23@gmail.com>
2024-09-08 19:31:48 +0600

    edits

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import { Float, OrbitControls } from '@react-three/drei'
+import { Float, OrbitControls, Text } from '@react-three/drei'
 import { Shattered } from './Shattered'
 
 const Experience = () => {
 	return (
 		<>
+		<Float>
+
+		<Text maxWidth={1} font='/BrokenGlass.ttf' textAlign='center' position={[0,0,-2]}>
+			Shattered Glass
+		</Text>
+		</Float>
 			<Float rotationIntensity={2}>
 				<Shattered />
 			</Float>
