4d4eb19497c384722c2a389d581fd34e16a642c9
SudeeptoBose <sudeeptobose23@gmail.com>
2024-09-08 19:23:36 +0600

    edits

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -6,9 +6,6 @@ import { Shattered } from './Shattered'
 const Experience = () => {
 	return (
 		<>
-			<OrbitControls enableZoom={false} />
-			<ambientLight />
-			{/* <Model scale={[2,2,2]} rotation={[0,Math.PI,0]} position={[0,-2,0]}/> */}
 			<Float rotationIntensity={2}>
 				<Shattered />
 			</Float>
